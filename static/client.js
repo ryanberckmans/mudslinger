ddabfe0d8b2bb5ca286717ef2fff881d7d6fef37
Vodur <devnull@localhost>
2016-02-16 01:16:14 -0500

    Basic chat window, needs a lot of help.

--- static/client.js
+++ static/client.js
@@ -3,7 +3,7 @@ var socket;
 var output_buffer='';
 var chat_buffer='';
 var rx;
-var mxp_open=false;
+var get_mxp_tag=false;
 var redirect_to_chat=false;
 function handle_data(msg) {
     //output_buffer+=msg.data
@@ -14,42 +14,57 @@ function handle_data(msg) {
 
     if (rx.length < 1) { return; }
 
-    if (mxp_open == true) {
-
-
+    /*
+    if ( get_mxp_tag ) {
+        
     }
-    else {
-        for ( i=0 ; i<rx.length ; i++ ) {
-            if (rx[i] == '\033') {
-                // Check if MXP escape (\0331z)
-                // if not long enough to chec, wait til it is
-                var seq = rx.slice(i);
-                if (seq.length < 4) { 
-                    rx = seq; 
-                    return; 
-                }
 
-                if (seq.slice(0,3) == '\033[1z') {
-                    mxp_open = true;
-                    rx = seq.slice(4);
-                    handle_data({data: ''});
-                    return;
-                }
+    for ( i=0 ; i<rx.length ; i++ ) {
+        if (rx[i] == '\033') {
+            // Check if MXP escape (\0331z)
+            // if not long enough to chec, wait til it is
+            var seq = rx.slice(i);
+            if (seq.length < 4) { 
+                rx = seq; 
+                return; 
             }
-                
-            if (rx[i] == '<') {
-                output_buffer += '&lt;';
-            } else if (rx[i] == '>') {
-                output_buffer += '&gt;';
-            } else {
-                output_buffer += rx[i];
+
+            if (seq.slice(0,3) == '\033[1z') {
+                mxp_open = true;
+                rx = seq.slice(4);
+                handle_data({data: ''});
+                return;
             }
         }
-        rx = '';
+            
+        if (rx[i] == '<') {
+            output_buffer += '&lt;';
+        } else if (rx[i] == '>') {
+            output_buffer += '&gt;';
+        } else {
+            output_buffer += rx[i];
+        }
+    }
+    rx = '';
+*/
+    if (rx.match(/\x1b\[1z/)) {
+        console.log("matched MXP");
+        var m = rx.match(/\x1b\[1z<DEST Comm>\x1b\[7z([^<]*)/);
+        console.log( 'm: ' + m );
+        if ( m ) {
+            console.log("matched dest");
+            chat_buffer += m[1];
+            var chat_buffer_raw = chat_buffer.replace(/\n\r/g, "<br>");
+            var chat_html = ansi_up.ansi_to_html(chat_buffer_raw);
+            $('#win_chat').html(chat_html);
+            $('#win_chat').scrollTop($('#win_chat').prop("scrollHeight"));
+        }
     }
 
     //output_buffer += "|]";
-    var output_raw=output_buffer.replace(/\n\r/g, "<br>")
+    output_buffer += rx;
+    rx = '';
+    var output_raw=output_buffer.replace(/\n\r/g, "<br>");
     var output_html=ansi_up.ansi_to_html(output_raw);
 
     var win_output=$('#win_output');
