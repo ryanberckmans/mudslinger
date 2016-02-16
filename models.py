48cce79166d23bfb5af23ff194906c93c072767d
Vodur <devnull@localhost>
2016-02-15 19:05:14 -0500

    Add models.py

---
+++ models.py
@@ -0,0 +1,32 @@
+from app import db
+from app import bcrypt
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String)
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<name {}'.format(self.name)
+
