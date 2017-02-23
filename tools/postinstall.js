a0ac913815a308cd111dbb664648ba6f62105d25
Odoth <devnull@localhost>
2017-02-22 21:04:54 -0800

    Get codemirror and jqwidgets from npm.

--- tools/postinstall.js
+++ tools/postinstall.js
@@ -1,4 +1,4 @@
-let fs = require("fs");
+let fs = require("fs-extra");
 
 let flnameConfigClient = "configClient.js"
 let flnameConfigClientDefault = "configClient.default.js"
@@ -9,6 +9,16 @@ let flnameConfigServerDefault = "configServer.default.js"
 // To be run from package root, paths accordingly
 fs.createReadStream("node_modules/socket.io-client/dist/socket.io.min.js").pipe(fs.createWriteStream('static/socket.io.min.js'));
 fs.createReadStream("node_modules/jquery/dist/jquery.min.js").pipe(fs.createWriteStream('static/jquery.min.js'));
+
+fs.copySync("node_modules/jqwidgets-framework/jqwidgets", "static/jqwidgets");
+
+fs.copySync("node_modules/codemirror/addon", "static/codemirror/addon");
+fs.copySync("node_modules/codemirror/keymap", "static/codemirror/keymap");
+fs.copySync("node_modules/codemirror/lib", "static/codemirror/lib");
+fs.copySync("node_modules/codemirror/mode", "static/codemirror/mode");
+fs.copySync("node_modules/codemirror/theme", "static/codemirror/theme");
+
+fs.copySync("node_modules/codemirror/LICENSE", 'static/codemirror/LICENSE');
 
 // Don't want to overwrite existing config file if any
 if (!fs.existsSync(flnameConfigClient)) {
